d3a92355f2d4bfb850e9c16f42d593c3b80854b9
manoj <gandhamanojkutty@gmail.com>
2023-08-23 20:14:16 +0530

    parameter=String

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test1"
-  value = "hello from test1 parameter"
-  type        = "SecureString"
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type        = "String"
   key_id = "9242f01f-6223-4487-a1e0-4607d8e4ee3d"
 }
