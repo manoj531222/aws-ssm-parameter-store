cf7e115bbfb8a205663bf849b760b5c291f1d1b4
manoj <gandhamanojkutty@gmail.com>
2023-08-25 13:55:10 +0530

    passwords updated separatly

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ resource "aws_ssm_parameter" "passwords" {
   count = length(var.passwords)
   name  = var.passwords[count.index].name
   value = var.passwords[count.index].value
-  type        = "secureString"
+  type        = "SecureString"
   key_id = "9242f01f-6223-4487-a1e0-4607d8e4ee3d"
 }
